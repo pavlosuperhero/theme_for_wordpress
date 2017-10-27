6ea2ad8b715b11c7d6fa609c60992070f529db1f
pavlosuperhero <pavlo.fabuliak@gmail.com>
2017-10-27 16:10:39 +0300

    45% done

--- english_school/index.php
+++ english_school/index.php
@@ -36,18 +36,16 @@ get_header(); ?>
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
+			echo '<div class="single-post">';
 				get_template_part( 'template-parts/content', get_post_format() );
-
+				
+			echo '</div>';
 			endwhile;
-
-			the_posts_navigation();
-
 		else :
 
 			get_template_part( 'template-parts/content', 'none' );
 
 		endif; ?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
--- english_school/page.php
+++ english_school/page.php
@@ -28,7 +28,6 @@ get_header(); ?>
 
 			endwhile; // End of the loop.
 			?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
